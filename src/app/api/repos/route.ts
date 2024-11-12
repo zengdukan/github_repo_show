228dbfc28122d21fad8d25c71d1316363568a4c1
zengdukan <zengdukan@163.com>
2024-11-12 11:04:45 +0800

    feat(app): add api repos

---
+++ src/app/api/repos/route.ts
@@ -0,0 +1,23 @@
+import { type NextRequest } from 'next/server'
+import { Repos } from '@/lib/mock_data'
+
+enum AccountType {
+    USERS = 'users',
+    ORGS = 'orgs',
+};
+
+export async function GET(request: NextRequest) {
+    const searchParams = request.nextUrl.searchParams;
+    const account_type = searchParams.get('account_type') ?? AccountType.USERS;
+    const project = searchParams.get('project') ?? '';
+    const mock = searchParams.get('mock') ?? '0';
+
+    if (mock === '1') {
+        return Response.json({ data: JSON.parse(Repos) });
+    } else {
+        const url = `https://api.github.com/${account_type}/${project}/repos`;
+        const res = await fetch(url);
+        const data = await res.json();
+        return Response.json({ data });
+    }
+}
